d6195622cac15b014c05e83038e28246195f4798
Roseane Tamires <rosetamires@hotmail.com>
2020-05-22 11:34:33 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAtleta/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ProjetoAtleta
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_altura = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.lbl_text = new System.Windows.Forms.Label();
+            this.lbl_imc = new System.Windows.Forms.Label();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 25);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(250, 20);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(12, 78);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(250, 20);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(12, 131);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(250, 20);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(12, 184);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(250, 20);
+            this.txtIdade.TabIndex = 3;
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(117, 9);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(35, 13);
+            this.lbl_nome.TabIndex = 4;
+            this.lbl_nome.Text = "Nome";
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(117, 168);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(34, 13);
+            this.lbl_idade.TabIndex = 5;
+            this.lbl_idade.Text = "Idade";
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(119, 115);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(31, 13);
+            this.lbl_peso.TabIndex = 6;
+            this.lbl_peso.Text = "Peso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // lbl_altura
+            // 
+            this.lbl_altura.AutoSize = true;
+            this.lbl_altura.Location = new System.Drawing.Point(117, 62);
+            this.lbl_altura.Name = "lbl_altura";
+            this.lbl_altura.Size = new System.Drawing.Size(34, 13);
+            this.lbl_altura.TabIndex = 8;
+            this.lbl_altura.Text = "Altura";
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(263, 25);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(118, 43);
+            this.btn_calc.TabIndex = 9;
+            this.btn_calc.Text = "Calcular IMC";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // lbl_text
+            // 
+            this.lbl_text.AutoSize = true;
+            this.lbl_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_text.Location = new System.Drawing.Point(268, 131);
+            this.lbl_text.Name = "lbl_text";
+            this.lbl_text.Size = new System.Drawing.Size(105, 20);
+            this.lbl_text.TabIndex = 10;
+            this.lbl_text.Text = "IMC é igual a:";
+            // 
+            // lbl_imc
+            // 
+            this.lbl_imc.AutoSize = true;
+            this.lbl_imc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_imc.Location = new System.Drawing.Point(294, 184);
+            this.lbl_imc.Name = "lbl_imc";
+            this.lbl_imc.Size = new System.Drawing.Size(54, 25);
+            this.lbl_imc.TabIndex = 11;
+            this.lbl_imc.Text = "0.00";
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Location = new System.Drawing.Point(265, 78);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(116, 43);
+            this.btn_enviar.TabIndex = 12;
+            this.btn_enviar.Text = "Enviar Dados";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            this.btn_enviar.Click += new System.EventHandler(this.btn_enviar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 225);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.lbl_imc);
+            this.Controls.Add(this.lbl_text);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.lbl_altura);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.lbl_nome);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtNome);
+            this.Name = "frmPrincipal";
+            this.Text = "Calculo do IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_altura;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Label lbl_text;
+        private System.Windows.Forms.Label lbl_imc;
+        private System.Windows.Forms.Button btn_enviar;
+    }
+}
+
---
+++ ProjetoAtleta/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoAtleta
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        Atleta obj = new Atleta();
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dados Obtidos: " + obj.ImprimeDados() + "\nIMC: " + obj.CalcularIMC().ToString());
+            lbl_imc.Text = obj.CalcularIMC().ToString("0.00");
+            
+        }
+
+        private void btn_enviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                obj.Nome = txtNome.Text;
+                obj.Altura = double.Parse(txtAltura.Text);
+                obj.Idade = int.Parse(txtIdade.Text);
+                obj.Peso = double.Parse(txtPeso.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Erro de execução.\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }     
+     
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
